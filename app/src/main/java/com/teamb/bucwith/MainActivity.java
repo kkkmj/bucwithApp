00d8feb5254ac2a8378709d4b33e98e3aa869faa
kkkmj <wjsdus531@naver.com>
2022-12-17 05:25:16 +0900

    흰화면(SSL)설정

--- app/src/main/java/com/teamb/bucwith/MainActivity.java
+++ app/src/main/java/com/teamb/bucwith/MainActivity.java
@@ -1,7 +1,9 @@
 package com.teamb.bucwith;
 
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.Window;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -20,10 +22,31 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView)findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
+        webView.setWebViewClient(new WebViewClientClass());
+        webSettings.setSupportMultipleWindows(false); // 여러 창 또는 탭 열리는 것 비허용
+        webSettings.setLoadWithOverviewMode(true); // 페이지 내에서만 이동하게끔
+        webSettings.setUseWideViewPort(true); // 페이지를 웹뷰 width에 맞춤
+        webSettings.setSupportZoom(false); // 확대 비활성화
+        webSettings.setBuiltInZoomControls(false); // 확대 비활성화
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 캐시 사용안함 (매번 새로 로딩)
+        webSettings.setDomStorageEnabled(true);
         // 앱에서 표시할 url 입력
         webView.loadUrl("http://bucwiths.shop/");
         webView.setWebViewClient(new WebViewClient());
+    }
+    private class WebViewClientClass extends WebViewClient {
+        // SSL 인증서 무시
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+
+        // 페이지 내에서만 url 이동하게끔 만듬
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 
     //폰의 뒤로가기 버튼의 동작 입력
